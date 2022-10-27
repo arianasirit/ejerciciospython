58ffbcf81143894391e1ea51cc72956fd3462625
carolo180 <caponeris1@gmail.com>
2022-10-27 12:08:27 -0500

    ejercicio isla del tesoro

---
+++ isladeltesoro.py
@@ -0,0 +1,25 @@
+isla del tesoro
+print("bienvenido a la isla, tu mision sera encontrar el tesoro")
+print("+++++++++++++++++++++++++++++++")
+print("para seguir adelante elije un camino,¿cual elijes:´derecha´o´izquierda´?")
+camino=input("introduce el camino elejido:" )
+if camino =="derecha ":
+    print("zona misteriosa")
+    print("caistes en un agujero, ´GAME OVER´")
+elif camino== "izquierda":
+    print("entrastes a un lago, ¿nadar o bordear?")
+    lago= input("introduce tu decision:")
+if  lago== "nadar":
+    print("atacado por pirañas,´ǴAME OVER´")
+elif lago== "bordear":
+    print("encontrastes dos puertas")
+    puerta=int(imput("¿cual elijes la puerta uno o la puerta dos?:"))
+if puerta== 1
+   print("entrastes en un cuarto llenon de monedas de oro")
+   print("felicitaciones encontrastes el tesoro´YOU WIN´")
+elif puerta==2:
+   print("te lleva a un volcan 'GAME OVER'")
+   else:
+       print("la puerta que eligistes no esta disponible")
+   else:
+       print("el camino elegido no esta disponible")
